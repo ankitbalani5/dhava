42d4c8d1dcfd5d2cb8fd716c103eb70afbe60dfb
Arun_dev <raosingh024@gmail.com>
2025-09-24 16:52:11 +0530

    delete challenges branch

--- lib/bloc/activityBloc/activity_state.dart
+++ lib/bloc/activityBloc/activity_state.dart
@@ -9,6 +9,7 @@ final class FeedSuccess extends ActivityState {
   final feed.FeedModel feedModel;
   FeedSuccess(this.feedModel);
 }
+
 final class FeedError extends ActivityState {
   final String error;
   FeedError(this.error);
