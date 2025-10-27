a266cb0c8571c44932a1896c81c9e8f9ee4a6c5e
AnkitCoherent <ankitbalani17@gmail.com>
2025-10-27 14:01:55 +0530

    latest code

--- lib/ui/profileScreens/profileScreen.dart
+++ lib/ui/profileScreens/profileScreen.dart
@@ -1013,7 +1013,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                           onTap: () {
                                             // Navigator.push(context, MaterialPageRoute(builder: (context) => SettingScreen()));
 
-                                            final link = "https://play.google.com/store/apps/details?id=${}";
+                                            final link = "https://play.google.com/store/apps/details?id=com.app.coherent_endurance";
 
                                             Share.share(
                                               "Check out my profile on Dhava 🏃‍♂️:\n$link",
