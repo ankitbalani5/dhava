a1489974385abd1de97d29a9828718709ffb1718
coherentlab <clabdeveloperteam@gmail.com>
2025-12-31 13:33:33 +0530

    package name change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         keystoreProperties.load(FileInputStream(keystorePropertiesFile))
     }
 
-    namespace = "com.coherent.dhava"
+    namespace = "com.coherentlab.dhava"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -36,7 +36,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.coherent.dhava"
+        applicationId = "com.coherentlab.dhava"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutterVersionCode
@@ -114,7 +114,7 @@ android {
         keystoreProperties.load(java.io.FileInputStream(keystorePropertiesFile))
     }
 
-    namespace = "com.coherent.dhava"
+    namespace = "com.coherentlab.dhava"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -129,7 +129,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.coherent.dhava"
+        applicationId = "com.coherentlab.dhava"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/app/coherent_endurance/MainActivity.kt
+++ android/app/src/main/kotlin/com/app/coherent_endurance/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.coherent.dhava
+package com.coherentlab.dhava
 
 import io.flutter.embedding.android.FlutterActivity
 
